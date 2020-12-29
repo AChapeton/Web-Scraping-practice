01999a7a05b3d9853c1646d46c83654973bcc773
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-12-29 15:41:06 -0600

    Validacion para paginacion

--- script.js
+++ script.js
@@ -13,13 +13,23 @@ async function run() {
 
   const reviews = [];
 
-  async function getPageData() {
+  async function getPageData(pageNumber = 1) {
     // Navegar a una página
     await page.goto('https://platzi.com/cursos/html5-css3/opiniones/1');
 
-    //Forma de recoger la data del sitio web
+    // Forma de recoger la data del sitio web
     const data = await page.evaluate(() => {
       const $reviews = document.querySelectorAll('.Review');
+
+      // Obtener elemento de paginación
+      const $pagination = document.querySelectorAll(
+        '.Pagination .Pagination-number'
+      );
+
+      // Total de páginas
+      const totalPages = Number(
+        $pagination[$pagination.length - 1].textContent.trim()
+      );
       const data = [];
 
       $reviews.forEach(($review) => {
@@ -31,9 +41,19 @@ async function run() {
       });
       return {
         reviews: data,
+        totalPages,
       };
     });
-    console.log(data);
+
+    console.log(`page ${pageNumber} of ${data.totalPages} completed`);
+
+    if (pageNumber <= data.totalPages) {
+      getPageData(pageNumber + 1);
+    } else {
+      await browser.close();
+    }
+
+    // console.log(data);
   }
 
   // Screenshot a la página
