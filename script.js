abaa265de522187edb2ce0e89c231b0754ae39c4
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-12-29 15:31:23 -0600

    Obteniendo data de reviews

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
+/* eslint-disable no-console */
 /* eslint-disable linebreak-style */
 const puppeteer = require('puppeteer');
 
-//Casi todas las funcionalidades de puppeteer devuelven una Promesa
+// Casi todas las funcionalidades de puppeteer devuelven una Promesa
 
 async function run() {
-  //Abrir navegador
+  // Abrir navegador
   const browser = await puppeteer.launch();
 
-  //Abrir nueva página (tab)
+  // Abrir nueva página (tab)
   const page = await browser.newPage();
 
-  //Navegar a una página
-  await page.goto('https://platzi.com/cursos/html5-css3/opiniones/1');
+  const reviews = [];
 
-  //Screenshot a la página
-  await page.screenshot({
-    path: 'screenshot.png',
-    fullPage: true,
-  });
+  async function getPageData() {
+    // Navegar a una página
+    await page.goto('https://platzi.com/cursos/html5-css3/opiniones/1');
+
+    //Forma de recoger la data del sitio web
+    const data = await page.evaluate(() => {
+      const $reviews = document.querySelectorAll('.Review');
+      const data = [];
+
+      $reviews.forEach(($review) => {
+        data.push({
+          content: $review
+            .querySelector('.Review-description')
+            .textContent.trim(),
+        });
+      });
+      return {
+        reviews: data,
+      };
+    });
+    console.log(data);
+  }
+
+  // Screenshot a la página
+  // await page.screenshot({
+  //   path: 'screenshot.png',
+  //   fullPage: true,
+  // });
+
+  getPageData();
+  // Cerrar navegador
+  // await browser.close();
 }
 
 run();
